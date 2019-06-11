b8cc135c23c45905294bb2e235fc420832a077a4
crperz <crperz@outlook.com>
2019-06-10 23:15:47 -0500

    Add ion-list

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -3,13 +3,29 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
-  styleUrls: ['./inicio.page.scss'],
+  styleUrls: ['./inicio.page.scss']
 })
 export class InicioPage implements OnInit {
+  pages: Page[] = [
+    {
+      icon: 'appstore',
+      name: 'Alert',
+      redirectTo: '/alert'
+    },
+    {
+      icon: 'logo-apple',
+      name: 'Action Sheet',
+      redirectTo: '/action-sheet'
+    }
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
+}
 
+interface Page {
+  icon: string;
+  name: string;
+  redirectTo: string;
 }
