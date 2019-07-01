2299f99ea18ef9538758befa548bbfc74c40843a
crperz <Cristian Prez>
2019-07-01 01:56:46 -0500

    Add toast message

--- src/app/pages/list/list.page.ts
+++ src/app/pages/list/list.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { DataService } from 'src/app/services/data.service';
 import { Observable } from 'rxjs';
-import { IonList } from '@ionic/angular';
+import { IonList, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-list',
@@ -12,18 +12,29 @@ export class ListPage implements OnInit {
   @ViewChild('list') list: IonList;
   users: Observable<any>;
 
-  constructor(private dataService: DataService) {}
+  constructor(
+    private dataService: DataService,
+    private toastCtrl: ToastController
+  ) {}
 
   ngOnInit() {
     this.users = this.dataService.getUsers();
   }
 
+  async presentToast(message: string) {
+    const toast = await this.toastCtrl.create({
+      message,
+      duration: 2000
+    });
+    toast.present();
+  }
+
   delete(user) {
-    console.log('Delete', user);
+    this.presentToast('Delete');
   }
 
   like(user) {
-    console.log('Like', user);
+    this.presentToast('Favorite');
     // Close sliding
     this.list.closeSlidingItems();
   }
